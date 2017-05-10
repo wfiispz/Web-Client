040a70457d407e486b1ab950fb36a460e1a308e3
Szymonqs <szymonqs@gmail.com>
2017-05-10 18:43:27 +0200

    # Web App - hosts

--- PZ/WebClient/models.py
+++ PZ/WebClient/models.py
@@ -6,3 +6,9 @@ class Monitors(models.Model):
     monitor_name = models.CharField(max_length=200)
     monitor_domain = models.URLField()
     user_id = models.ForeignKey(User)
+
+class Hosts(models.Model):
+    monitor_id_id = models.IntegerField()
+    host_name = models.CharField(max_length=200)
+    host_description = models.CharField(max_length=200)
+    host_measurements = models.CharField(max_length=200)
