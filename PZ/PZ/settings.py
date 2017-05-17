f02d4c1380eaf9ff70ae5b012ecb97df08f55798
2korzeniak <Michal.Korzeniak@fis.agh.edu.pl>
2017-05-17 16:08:55 +0200

    Django settings updated and ALLOWED_HOST parameter set (security issues)

--- PZ/PZ/settings.py
+++ PZ/PZ/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$^2_z%#h^5o45(&4wfkyzpyj7g3$1v8l8v*ih!@u^m3=-^h-#q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','wfiss-web.herokuapp.com']
 
 # Application definition
 
