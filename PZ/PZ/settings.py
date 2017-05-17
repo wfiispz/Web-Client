2b1691eee3011c2059aedc175b5e288f7de035f3
2korzeniak <Michal.Korzeniak@fis.agh.edu.pl>
2017-05-17 15:42:14 +0200

    Settings file for djago app update

--- PZ/PZ/settings.py
+++ PZ/PZ/settings.py
@@ -119,4 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
