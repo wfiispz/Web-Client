a2d9ab66ffbdb0d8e2c1558650ef8149335cdb1f
Sekobb <majewski.jakub@tlen.pl>
2017-05-18 20:11:07 +0200

    Added communicate about invalid credentials

--- PZ/WebClient/views.py
+++ PZ/WebClient/views.py
@@ -81,18 +81,18 @@ def logout(request):
 def register_success(request):
     return render_to_response('register_success.html')
 
-
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/register_success/")
+    else:
+        form = UserCreationForm()
 
     args = {}
     args.update(csrf(request))
-
-    args['form'] = UserCreationForm()
+    args['form'] = form
 
     return render_to_response('register.html', args)
 
