ddd68c09bc6b9ff53f70a5e4dea6ae5fc5c44130
Szymonqs <szymonqs@gmail.com>
2017-05-24 14:28:12 +0200

    # Web App - Charts

--- PZ/WebClient/views.py
+++ PZ/WebClient/views.py
@@ -5,6 +5,10 @@ from django.template.context_processors import csrf
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .Connector import *
+from django.shortcuts import render
+from graphos.sources.simple import SimpleDataSource
+
+from graphos.renderers.yui import LineChart
 
 
 def index(request):
@@ -81,6 +85,7 @@ def logout(request):
 def register_success(request):
     return render_to_response('register_success.html')
 
+
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
@@ -106,23 +111,48 @@ def hosts(request, monitor_id):
         c._payload = {"name": search_query}
 
     host_list, page = c.get_resources()
-    return render_to_response('hosts.html', {"get_name": search_query, "full_name": request.user.username, 'monitor_domain' : current_monitor.monitor_domain, 'monitor_id' : monitor_id, 'host_list' : host_list})
+    return render_to_response('hosts.html', {"get_name": search_query, "full_name": request.user.username,
+                                             'monitor_domain': current_monitor.monitor_domain, 'monitor_id': monitor_id,
+                                             'host_list': host_list})
+
+
+def archives(request, monitor_id):
+    current_monitor = Monitors.objects.get(id=monitor_id)
+    c = Connector(urljoin(current_monitor.monitor_domain, 'resources'))
+
+    host_list, page = c.get_resources()
+    measurements_list = []
+    for host in host_list:
+        m1 = host.measurements
+        measurements_list = measurements_list + c.get_measurements(str(m1).replace("\'", "\""))
+
+    for measurement in measurements_list:
+        value = str(measurement.values)
+        host = str(measurement.host)
+        measurement.values = value.split('/')[len(value.split('/')) - 2]
+        measurement.host = host.split('/')[4]
+
+    return render_to_response('archives.html', {'host_list': host_list, 'measurements_list': measurements_list,
+                                                'monitor_id': monitor_id})
 
 
 def measurements(request, monitor_id, host_id):
-    c = Connector(Monitors.objects.get(id = monitor_id).monitor_domain)
+    c = Connector(Monitors.objects.get(id=monitor_id).monitor_domain)
 
     measurements_endpoints = c.get_resource_id('resources/' + host_id).measurements
     measurements_list = c.get_measurements(str(measurements_endpoints).replace("\'", "\""))
 
     for measurement in measurements_list:
-        value =  str(measurement.values)
+        value = str(measurement.values)
         measurement.values = value.split('/')[len(value.split('/')) - 2]
 
-    return render_to_response('measurements.html', {"full_name": request.user.username, 'resources_list' : measurements_list, 'monitor_id' : monitor_id, 'host_id' : host_id})
+    return render_to_response('measurements.html',
+                              {"full_name": request.user.username, 'resources_list': measurements_list,
+                               'monitor_id': monitor_id, 'host_id': host_id})
+
 
 def values(request, monitor_id, host_id, measurement_id):
-    c = Connector(Monitors.objects.get(id = monitor_id).monitor_domain)
+    c = Connector(Monitors.objects.get(id=monitor_id).monitor_domain)
 
     measurements_endpoints = c.get_resource_id('resources/' + host_id).measurements
     for endpoint in measurements_endpoints:
@@ -132,4 +162,57 @@ def values(request, monitor_id, host_id, measurement_id):
     values_list = c.get_measurement_values(measurements_endpoint)
     print(values_list)
 
-    return render_to_response('values.html', {"full_name": request.user.username, 'values_list' : values_list})
+    return render_to_response('values.html', {"full_name": request.user.username, 'values_list': values_list})
+
+
+def graph(request, monitor_id, host_id, measurement_id):
+    c = Connector(Monitors.objects.get(id=monitor_id).monitor_domain)
+
+    measurements_endpoints = c.get_resource_id('resources/' + host_id).measurements
+    for endpoint in measurements_endpoints:
+        if endpoint.__contains__(measurement_id):
+            measurements_endpoint = endpoint
+
+    values_list = c.get_measurement_values(measurements_endpoint)
+
+    data = [
+        ['DateTime', 'values']
+    ]
+
+    for val in values_list:
+        data.append([val.datetime, val.value])
+
+    data_source = SimpleDataSource(data=data)
+
+    chart = LineChart(data_source)
+
+    return render_to_response('graph.html',
+                              {"full_name": request.user.username, 'values_list': values_list, 'monitor_id': monitor_id,
+                               'host_id': host_id, 'measurement_id': measurement_id, 'chart': chart})
+
+
+def update_graph(request, monitor_id, host_id, measurement_id):
+    c = Connector(Monitors.objects.get(id=monitor_id).monitor_domain)
+
+    measurements_endpoints = c.get_resource_id('resources/' + host_id).measurements
+    for endpoint in measurements_endpoints:
+        if endpoint.__contains__(measurement_id):
+            measurements_endpoint = endpoint
+
+    values_list = c.get_measurement_values(measurements_endpoint)
+
+    data = [
+        ['DateTime', 'values']
+    ]
+
+    for val in values_list:
+        data.append([val.datetime, val.value])
+
+    data_source = SimpleDataSource(data=data)
+
+    chart = LineChart(data_source)
+
+    chart.get_options_json = '{"title": "Chart"}'
+    return render(request, 'update_graph.html',
+                  {"full_name": request.user.username, 'values_list': values_list, 'monitor_id': monitor_id,
+                   'host_id': host_id, 'measurement_id': measurement_id, 'chart': chart})
